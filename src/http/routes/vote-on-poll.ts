3d534ffea72964b500f5d1e219355feac4030a7d
jhuly ambrozini <jhulyamfebr@gmial.com>
2024-02-07 19:03:56 -0300

    feat: usando o redis para criação da pontuação das opções

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { prisma } from '../../lib/prisma';
 import { FastifyInstance } from 'fastify';
 import { randomUUID } from 'crypto';
+import { redis } from '../../lib/redis';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -36,6 +37,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         });
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({ message: 'Você já votou nesta opção' });
       }
@@ -59,6 +62,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     });
+
+    await redis.zincrby(pollId, 1, pollOptionId);
 
     return reply.status(201).send();
   });
