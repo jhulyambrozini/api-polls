ea9c9ddf0353d64fe68c7ada8552d4904e2f24e0
jhuly ambrozini <jhulyamfebr@gmial.com>
2024-02-07 16:30:54 -0300

    config: configurando o banco de dados

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,10 @@ import fastify from 'fastify';
 
 const app = fastify();
 
+app.get('/', () => {
+  return 'hellow';
+});
+
 app.listen({ port: 3000 }).then(() => {
   console.log('Server is running');
 });
