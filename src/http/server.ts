fdf88cd6880dc7b599b7df55c15ec7668871519a
jhuly ambrozini <jhulyamfebr@gmial.com>
2024-02-07 16:45:31 -0300

    feat: criando rota de post poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
 import fastify from 'fastify';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
 
-app.get('/', () => {
-  return 'hellow';
+const prisma = new PrismaClient();
+
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({ pollId: poll.id });
 });
 
 app.listen({ port: 3000 }).then(() => {
